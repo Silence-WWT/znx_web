211222dace6ee615e8ff2c63c7364f47e1dadcd3
zhaoliang <yourzhaozhao@gmail.com>
2014-10-14 00:22:20 +0800

    Init extensions. Typo

--- app/__init__.py
+++ app/__init__.py
@@ -13,3 +13,7 @@ login_manager.login_view = 'auth.login'
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    return app
