ba171a161aebca3a65c5c8eaf7dfcc9296e4275e
zhaoliang <yourzhaozhao@gmail.com>
2014-10-14 15:26:52 +0800

    add Authentication component:   Login and RegistrationForm, Login page. Update requirements.

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,10 @@
 from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.bootstrap import Bootstrap
 from config import config
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -13,7 +15,15 @@ login_manager.login_view = 'auth.login'
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+
+    bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
---
+++ app/auth/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from . import auth
+from .forms import LoginForm
+from ..models import User
+from flask.ext.login import login_user
+from flask import redirect, url_for, render_template, flash, request
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password.')
+    return render_template('auth/login.html', form=form)
