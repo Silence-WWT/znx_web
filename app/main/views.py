976119da62a62b91d802b3e5cc329979b541918b
zhaoliang <yourzhaozhao@gmail.com>
2014-10-21 21:56:27 +0800

    Update user login form.

--- app/main/views.py
+++ app/main/views.py
@@ -2,11 +2,13 @@
 from . import main
 from flask.ext.login import login_required, logout_user
 from flask import redirect, url_for, render_template, flash
+from ..user.forms import LoginForm as UserLoginForm
 
 
 @main.route('/login', methods=['GET'])
 def login():
-    return render_template('login_choose_py.html')
+    user_form = UserLoginForm()
+    return render_template('login_choose_py.html', user_form=user_form)
 
 
 @main.route('/logout')
--- app/user/forms.py
+++ app/user/forms.py
@@ -8,11 +8,10 @@ from ..models import User
 
 # TODO: add telephone number.
 class LoginForm(Form):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+    username = StringField('Email', validators=[DataRequired(), Length(1, 64),
                                              Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Log In')
 
 
 class RegistrationForm(Form):
