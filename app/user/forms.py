29f61d51a5469b4b98b9c39f4ea2edf228af4d7f
zhaoliang <yourzhaozhao@gmail.com>
2014-10-21 21:41:07 +0800

    Add username for user register.

--- app/user/forms.py
+++ app/user/forms.py
@@ -16,11 +16,14 @@ class LoginForm(Form):
 
 
 class RegistrationForm(Form):
+    username = StringField('username',
+                           validators=[DataRequired(),
+                                       Length(6,64, u'用户名长度不符合要求')])
     cellphone = StringField('cellphone',
                             validators=[
                                 DataRequired(u'必填'),
                                 Length(11, 11, u'手机号码不符合规范')])
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+    email = StringField('Email', validators=[Length(1, 64),
                                              Email()])
     password = PasswordField('Password', validators=[
         DataRequired(), EqualTo('password2', message='Passwords must match.')])
@@ -28,9 +31,17 @@ class RegistrationForm(Form):
 
     confirmed = BooleanField()
 
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError(u'用户名已经被占用')
+
     def validate_email(self, field):
         if User.query.filter_by(email=field.data).first():
-            raise ValidationError('Email already registered.')
+            raise ValidationError(u'邮箱已经被注册')
+
+    def validate_cellphone(self, field):
+        if User.query.filter_by(cellphone=field.data).first():
+            raise ValidationError(u'手机号已经被注册')
 
     def validate_confirmed(self, field):
         if field.data == False:
