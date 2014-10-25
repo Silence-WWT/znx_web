e59570244c24ca504ab8e2f41e948ee0204003a2
15029269197 <tw93@qq.com>
2014-10-25 18:42:23 +0800

    time

--- app/static/js/time.js
+++ app/static/js/time.js
@@ -1,18 +1,18 @@
 var wait=60;
 $("#regmessage").attr('disabled',false);
 $("#regmessage").val("免费获取验证码");  
-function time(o) {
+function messagetime(o) {
         if (wait == 0) {
             o.removeAttribute("disabled");           
             o.value="免费获取验证码";
             wait = 60;
         } else {
 
-            o.setAttribute("disabled", true);
-            o.value="重新发送(" + wait + ")";
+          //  o.setAttribute("disabled", true);
+           // o.value="重新发送(" + wait + ")";
             wait--;
             setTimeout(function() {
-                time(o)
+               messagetime(o);
             },
             1000)
         }
@@ -20,13 +20,12 @@ function time(o) {
 	
 	$(function(){
 		$("#regmessage").click(function(){
-			time(this);
+			
 			var mobile=$("#inputPhone").val();
 			var isMobile=/^(?:13\d|15\d|18\d)\d{5}(\d{3}|\*{3})$/; //手机号码验证规则
 			var moblieurl=$("#mobileurl").val();
-
+             //messagetime(regmessage);
 			//alert(moblieurl);
-
 			if(isMobile.test(mobile)){
 			   $.ajax({
                     type: "POST", //用POST方式传输
@@ -34,9 +33,15 @@ function time(o) {
                     url: moblieurl, //目标地址
                     data: "mobile=" + mobile,
                     error: function (XMLHttpRequest, textStatus, errorThrown) { },
-                    success: function (msg){ }
+                    success: function (msg){
+						var regmessage=$("#regmessage");
+						 messagetime(regmessage);
+						 return false;
+					
+					}
 
-                });		
+                });	
+				
 			}
 		
 			})
