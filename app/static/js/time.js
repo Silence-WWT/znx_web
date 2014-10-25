70868c58642807a8bfce516436097953db309c2f
15029269197 <tw93@qq.com>
2014-10-25 21:11:10 +0800

    短信验证

--- app/static/js/time.js
+++ app/static/js/time.js
@@ -1,46 +1,42 @@
-var wait=60;
-$("#regmessage").attr('disabled',false);
-$("#regmessage").val("免费获取验证码");  
-function messagetime(o) {
-        if (wait == 0) {
-            o.removeAttribute("disabled");           
-            o.value="免费获取验证码";
-            wait = 60;
-        } else {
-
-          //  o.setAttribute("disabled", true);
-           // o.value="重新发送(" + wait + ")";
-            wait--;
-            setTimeout(function() {
-               messagetime(o);
-            },
-            1000)
-        }
-    }
-	
-		$("#regmessage").click(function(){
-			
-			var mobile=$("#inputPhone").val();
-			var isMobile=/^(?:13\d|15\d|18\d)\d{5}(\d{3}|\*{3})$/; //手机号码验证规则
-			var moblieurl=$("#mobileurl").val();
-             //messagetime(regmessage);
-			//alert(moblieurl);
-			if(isMobile.test(mobile)){
-			   $.ajax({
-                    type: "POST", //用POST方式传输
-                    dataType: "text", //数据格式:JSON
-                    url: moblieurl, //目标地址
-                    data: "mobile=" + mobile,
-                    error: function (XMLHttpRequest, textStatus, errorThrown) { },
-                    success: function (msg){
-						var regmessage=$("#regmessage");
-						 messagetime(regmessage);
-						 return false;
-					
-					}
-
-                });	
-				
-			}
-            return false;
-			})
+var InterValObj; //timer变量，控制时间
+var count = 60; //间隔函数，1秒执行
+var curCount;//当前剩余秒数
+function sendMessage() {
+	curCount = count;
+	var mobilephone=$("#inputPhone").val();//手机号码
+    var messageurl=$("#mobileurl").val();
+	if(mobilephone != ""){
+		//设置button效果，开始计时
+		$("#regmessage").attr("disabled", "true");
+		$("#regmessage").val("请在" + curCount + "秒内输入验证码");
+		InterValObj = window.setInterval(SetRemainTime, 1000); //启动计时器，1秒执行一次
+	//向后台发送处理数据
+		$.ajax({
+			type: "POST", //用POST方式传输
+			dataType: "text", //数据格式:JSON
+			url: messageurl, //目标地址
+			data: "mobile=" + mobilephone,
+			error: function (data) {
+           alert(data);
+           },
+		success: function (data){
+            alert(data);
+           }
+		});
+	}else{
+		//alert("手机号码不能为空！");
+	}
+}
+//timer处理函数
+function SetRemainTime() {
+	if (curCount == 0) {                
+		window.clearInterval(InterValObj);//停止计时器
+		$("#regmessage").removeAttr("disabled");//启用按钮
+		$("#regmessage").val("重新发送验证码");
+		code = ""; //清除验证码。如果不清除，过时间后，输入收到的验证码依然有效    
+	}
+	else {
+		curCount--;
+		$("#regmessage").val("请在" + curCount + "秒内输入验证码");
+	}
+}
