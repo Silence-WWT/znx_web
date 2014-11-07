95b457ed3221312ac3251393dd63a050422de556
silencewwt <silencewwt@gmail.com>
2014-11-08 01:33:24 +0800

    optimize api get_district_profession

--- app/api_1_0/get_location_profession.py
+++ app/api_1_0/get_location_profession.py
@@ -3,7 +3,7 @@ import json
 
 from flask import request
 
-from ..models import Organization, Location, City
+from ..models import Organization, Location, City, Profession
 from . import api
 from api_constants import *
 from utils import get_professions
@@ -16,14 +16,15 @@ def get_district_profession():
     if city:
         location_list = Location.query.filter_by(city_id=city.id)
         districts = [location.district for location in location_list]
-        location_id_list = [location.id for location in location_list]
-        organization_list = Organization.query.filter(Organization.location_id.in_(location_id_list))
-        profession_set = set()
-        for organization in organization_list:
-            for profession in get_professions(organization):
-                profession_set.add(profession)
-        professions = list(profession_set)
+        # location_id_list = [location.id for location in location_list]
+        # organization_list = Organization.query.filter(Organization.location_id.in_(location_id_list))
+        # profession_set = set()
+        # for organization in organization_list:
+        #     for profession in get_professions(organization):
+        #         profession_set.add(profession)
+        # professions = list(profession_set)
         data['districts'] = districts
-        data['professions'] = professions
+        # data['professions'] = professions
+        data['professions'] = [profession.profession for profession in Profession.query.all()]
     data['status'] = SUCCESS
     return json.dumps(data)
