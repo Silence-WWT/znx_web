5276260833f2bde47f43fb03b0156b1067e0917f
zhaoliang <yourzhaozhao@gmail.com>
2014-11-11 15:01:11 +0800

    Class photo upload.

--- config.py
+++ config.py
@@ -22,6 +22,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    PHOTO_DIR = os.path.join(basedir, 'photos')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'mysql+pymysql://dev:devpassword@localhost/znx?charset=utf8'
 
